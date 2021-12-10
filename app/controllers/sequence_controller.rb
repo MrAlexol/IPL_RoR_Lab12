733c5356a6fdd25d7c5bc5fc6899afedea52218d
MrAlexol <Alex02072002@yandex.ru>
2021-12-11 01:14:55 +0300

    Add everything. It works

---
+++ app/controllers/sequence_controller.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# Контроллер для последовательности
+class SequenceController < ApplicationController
+  def input
+  end
+
+  def view
+    @input = validate_input params[:values]
+    subs_arr = find_increasing_subs.filter { |arr| arr.count > 1 }
+    @subs = subs_arr.map { |subseq| subseq.join(' ') }
+    @result = subs_arr.max_by(&:length).join(' ')
+    @input = @input.join(' ')
+  rescue StandardError => e
+    @error = case e.class.to_s
+             when 'ArgumentError' then 'Введены посторонние символы'
+             when 'NoMethodError' then 'Последовательность не найдена'
+             else e
+             end
+  end
+
+  private
+
+  def find_increasing_subs
+    @input.each_with_object([[]]) do |el, acc|
+      acc << [] if acc[-1].any? && el <= acc[-1][-1]
+      acc[-1] << el
+    end
+  end
+
+  def validate_input(input)
+    raise 'Ошибочный GET-запрос' if input.nil?
+    raise 'Вы ничего не ввели' if input.empty?
+
+    result = input.split.map { |el| Integer el }
+    raise 'Последовательность короче 10 чисел' if result.length < 10
+
+    result
+  end
+end
